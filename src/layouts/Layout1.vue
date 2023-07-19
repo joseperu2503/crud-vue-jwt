64fda8fe7433e85994eff03ec3434688c6a2ff8a
Joseperez <51133263+joseperu2503@users.noreply.github.com>
2023-07-19 13:59:58 -0500

    correccion centrado vertical layout 1

--- src/layouts/Layout1.vue
+++ src/layouts/Layout1.vue
@@ -1,12 +1,15 @@
 <template>
-  <div class="bg-gray-50 fixed right-0 bottom-0 left-0 top-0 flex items-center">
-    <div class="flex gap-8 p-4 sm:p-10 w-full justify-center " :class="{'flex-row-reverse': slotPosition == 'left'}">
-      <div class="hidden lg:flex items-end justify-center">
-        <img :src="img" alt="" class="max-w-lg max-h-[500px]"/>
+  <div class="bg-gray-50 fixed right-0 bottom-0 left-0 top-0 overflow-scroll">
+    <div class="flex items-center min-h-full">
+      <div class="flex gap-8 p-4 sm:p-10 w-full justify-center " :class="{'flex-row-reverse': slotPosition == 'left'}">
+        <div class="hidden lg:flex items-end justify-center">
+          <img :src="img" alt="" class="max-w-lg max-h-[500px]"/>
+        </div>
+        <div class="flex items-center justify-center max-w-md w-full">
+          <slot />
+        </div>
       </div>
-      <div class="flex items-center justify-center max-w-md w-full">
-        <slot />
-      </div>
+
     </div>
   </div>
 </template>
